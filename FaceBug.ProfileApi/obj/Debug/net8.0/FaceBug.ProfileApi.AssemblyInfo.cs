236368656155341309f0a9f316686a1d701f115f
Jefferson Pinheiro <jeffersonpinheiro194@gmail.com>
2025-01-31 15:17:54 -0300

    First Commit

--- FaceBug.ProfileApi/obj/Debug/net8.0/FaceBug.ProfileApi.AssemblyInfo.cs
+++ FaceBug.ProfileApi/obj/Debug/net8.0/FaceBug.ProfileApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FaceBug.ProfileApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c83fa68ffc8527841dfcf4d897d66ffcbc5289")]
 [assembly: System.Reflection.AssemblyProductAttribute("FaceBug.ProfileApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FaceBug.ProfileApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
