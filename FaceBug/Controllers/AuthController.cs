657d308fdd91075e0383952cf71b70cff3302d46
Jefferson Pinheiro <jeffersonpinheiro194@gmail.com>
2025-03-18 21:52:14 -0300

    Create AuthController and Models

---
+++ FaceBug/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using FaceBug.Entities;
+using FaceBug.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaceBug.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult <string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+            if(new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password)
+                == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong user or password.");
+            }
+            string token = "success";
+
+            return Ok(token);
+        }
+    }
+}
